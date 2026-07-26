c9ec65ed026bc1c66a12bf5fe73468e5c1d1b5d2
Bongo Poyo <pineapple123132@gmail.com>
2026-07-26 17:56:05 +0500

    added yazi function as y

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,12 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
 fish_add_path /home/bongo/.spicetify
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
+end
